c8ff12cb61962d362a184bd11697ac2cdb2e160a
Lawrence <locaulker@gmail.com>
2023-01-05 06:48:01 -0500

    resume @ L147

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import App from "./App"
 // import Alternative from './Alternative';
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<App />)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
