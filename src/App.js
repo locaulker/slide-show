dc3769ac1843bbdc7000f3d04941f353eed44d3a
Lawrence O. Caulker <locaulker@gmail.com>
2023-01-05 16:00:30 -0500

    complete slider with auto slide change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,25 @@ import data from "./data"
 function App() {
   const [people, setPeople] = useState(data)
   const [index, setIndex] = useState(0)
-  console.log(people)
+  // console.log(people)
+
+  useEffect(() => {
+    const lastIndex = people.length - 1
+    if (index < 0) {
+      setIndex(lastIndex)
+    }
+    if (index > lastIndex) {
+      setIndex(0)
+    }
+  }, [index, people])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 4000)
+
+    return () => clearInterval(slider)
+  }, [index])
 
   return (
     <section className="section">
@@ -20,8 +38,19 @@ function App() {
           const { id, image, title, name, quote } = person
           // more stuff goes here
 
+          let position = "nextSlide"
+          if (personIndex === index) {
+            position = "activeSlide"
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide"
+          }
+
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
@@ -30,10 +59,10 @@ function App() {
             </article>
           )
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
--- src/index.js
+++ src/index.js
@@ -5,8 +5,4 @@ import App from "./App"
 // import Alternative from './Alternative';
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+root.render(<App />)
